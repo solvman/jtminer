957fe6b01fe5418ad3e7c2cad782a06e54eea65c
Phil Grim <pagrim@mistiqtech.com>
2018-12-19 12:05:01 -0500

    Use generic user and password in example.

--- jtminer.bat
+++ jtminer.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-java -jar target\jtminer-0.1-SNAPSHOT-jar-with-dependencies.jar --host localhost -port 10617 --user user --password password --coinbase-addr 47TZrzoftLodX1gjbsMEDZPufLjy6uf7jD
+java -jar target\jtminer-0.1-SNAPSHOT-jar-with-dependencies.jar --host localhost -port 10617 --user neo --password 22Monkeys! --coinbase-addr 47TZrzoftLodX1gjbsMEDZPufLjy6uf7jD

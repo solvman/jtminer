a81372964eb85cfcbe305169c2ed94f8817ea752
Phil Grim <pagrim@mistiqtech.com>
2018-12-19 12:02:59 -0500

    Added copyright notice.

--- src/main/java/live/thought/jtminer/Solver.java
+++ src/main/java/live/thought/jtminer/Solver.java
@@ -1,3 +1,25 @@
+/*
+ * jtminer Java mining software for the Thought Network
+ * 
+ * Copyright (c) 2018, Thought Network LLC
+ *
+ * Based on code from Cuckoo Cycle, a memory-hard proof-of-work
+ * Copyright (c) 2013-2016 John Tromp
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package live.thought.jtminer;
 
 import java.io.IOException;
